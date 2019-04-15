64dd27edd7d4e7301d26ea5d4f2843d6192af2fb
Vasilije Pantic <vpantic10@gmail.com>
2019-04-15 15:22:34 +0200

    Implemented editing hotel

--- spring.project.isamrs/src/main/java/tim3/spring/project/isamrs/controller/HotelController.java
+++ spring.project.isamrs/src/main/java/tim3/spring/project/isamrs/controller/HotelController.java
@@ -31,4 +31,30 @@ public class HotelController {
 		Hotel retVal = hotelService.create(new Hotel(hotelDTO));
 		return new ResponseEntity<>(retVal, HttpStatus.CREATED);
 	}
+	
+	@RequestMapping(value = "/findHotel", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Hotel> findRentacar() {
+		Hotel hotel = hotelService.getOne(1);
+		if (hotel != null) {
+			return new ResponseEntity<>(hotel, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@RequestMapping(value = "/saveChangesHotel", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Hotel> saveChangesRentACar(@RequestBody Hotel hotel) {
+		Hotel h = hotelService.getOne(hotel.getId());
+		if (h == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		h.setId(hotel.getId());
+		h.setName(hotel.getName());
+		h.setAddress(hotel.getAddress());
+		h.setPromotionalDescription(hotel.getPromotionalDescription());
+		h.setHotelCustomerServices(hotel.getHotelCustomerServices());
+
+		Hotel hotel2 = hotelService.save(h);
+		return new ResponseEntity<>(hotel2, HttpStatus.OK);
+	}
 }
